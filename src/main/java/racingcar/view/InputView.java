4f64847e151901b82ac840a9a175c1418d9733cc
seok <hyeongseok1995@gmail.com>
2025-10-22 17:03:27 +0900

    feat(InputView): 입력 및 입력 검증 기능 구현
    1. 자동차 이름 입력 기능 구현
    2. 시도할 횟수 입력 기능 구현
    3. 자동차 이름 입력 양식 검증 구현
    4. 이름 입력은 영어만 가능하도록 문자 검증 구현
    5. 시도 횟수는 양수만 가능하도록 검증 구현
    6. 시도 횟수 입력 시 숫자 아닌 값 입력에 대한 검증 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,13 +1,50 @@
 package racingcar.view;
 
-import camp.nextstep.edu.missionutils.Console;
+import static camp.nextstep.edu.missionutils.Console.*;
 
 public class InputView {
+
+    private static final String CAR_NAMES_REGEX = "^[a-zA-Z]+(\\s*,\\s*[a-zA-Z]+)*\\s*$";
+
     public String inputCarName() {
-        return Console.readLine();
+        String carNames = readLine().trim();
+        validateNameFormat(carNames);
+
+        return carNames;
     }
 
-    public String inputTryCount() {
-        return Console.readLine();
+    public int inputTryCount() {
+        String tryCount = readLine();
+        validateIntegerFormat(tryCount);
+
+        int count = stringToInt(tryCount);
+        validatePositiveNumber(count);
+
+        return count;
+    }
+
+    private void validatePositiveNumber(int tryCount) {
+        if (tryCount <= 0) {
+            throw new IllegalArgumentException("시도 횟수는 양의 정수를 입력해야 합니다.");
+        }
+    }
+
+    private void validateIntegerFormat(String tryCount) {
+        try {
+            stringToInt(tryCount);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("시도 횟수는 숫자를 입력해야 합니다.");
+        }
+    }
+
+    private void validateNameFormat(String carNames) {
+        if (carNames.matches(CAR_NAMES_REGEX)) {
+            return;
+        }
+        throw new IllegalArgumentException("자동차 이름 입력 양식을 다시 확인해주세요");
+    }
+
+    private int stringToInt(String tryCount) {
+        return Integer.parseInt(tryCount);
     }
 }
