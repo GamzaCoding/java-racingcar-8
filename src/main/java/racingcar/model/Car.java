e37e72da9ede18691a0091d6cf999839eec1afbb
seok <hyeongseok1995@gmail.com>
2025-10-23 10:57:11 +0900

    feat(Car): 전진 조건 인식 기능 구현
    1. Car 객체 내부 canMove() 메서드 구현

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,10 +1,16 @@
 package racingcar.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
 
     private final String name;
 
     public Car(String name) {
         this.name = name;
+    }
+
+    public boolean canMove() {
+        return Randoms.pickNumberInRange(0, 9) > 4;
     }
 }
